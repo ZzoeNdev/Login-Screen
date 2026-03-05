c3bb4ed7801b5b313985f5ab8c1297d5ab77364e
Enzzo <enzzokenzo@gmail.com>
2026-03-04 23:47:16 -0300

    final commit (finalizing codes)

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const divNoLogin = document.getElementById("semloginContainer")
-const buttonLogin = document.getElementById("loginButton")
+const newButton = document.getElementById("newButton")
 const h1 = document.getElementById("h1")
+const containerL = document.getElementById("loginContainer")
+const containerS = document.getElementById("signContainer")
 
-buttonLogin.addEventListener("click", function(){
+newButton.addEventListener("click", function(){
+
     if (divNoLogin.classList.contains("open")){
+
         divNoLogin.classList.remove("open")
         divNoLogin.classList.add("close")
+        containerS.classList.add("dragLeftWithLogin")
         h1.classList.add("fadeIn")
         h1.style.textAlign = "end"
-        setTimeout(function(){h1.classList.remove("fadeIn")}, 1000)
+        setTimeout(function(){h1.classList.remove("fadeIn");containerS.classList.remove("dragLeftWithLogin");containerL.classList.remove("dragRightWithLogin");}, 800)
         h1.innerText = "Ainda não tem cadastro?"
-        buttonLogin.innerText = "Cadastre-se agora!"
+        newButton.innerText = "Cadastre-se agora!"
+        containerL.classList.add("dragRightWithLogin")
     }
+
     else{
+
         divNoLogin.classList.remove("close")
         divNoLogin.classList.add("open")
+        containerL.classList.add("dragRightNoLogin")
         h1.classList.add("fadeIn")
         h1.style.textAlign = "start"
-        setTimeout(function(){h1.classList.remove("fadeIn")}, 1000)
+        setTimeout(function(){h1.classList.remove("fadeIn");containerL.classList.remove("dragRightNoLogin");containerS.classList.remove("dragLeftNoLogin");}, 800)
         h1.innerText = "Já possui cadastro?"
-        buttonLogin.innerText = "Entrar em sua conta!"
+        newButton.innerText = "Entrar em sua conta!"
+        containerS.classList.add("dragLeftNoLogin")
     }
+
 });
